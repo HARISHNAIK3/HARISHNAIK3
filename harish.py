553005e9cadc739d5f47a6519ec8f40c9c2fa822
HARISHNAIK3 <158150989+HARISHNAIK3@users.noreply.github.com>
2024-01-30 16:10:24 +0530

    Add files via upload

---
+++ harish.py
@@ -0,0 +1,27 @@
+print("harish")
+#sum of numbers
+#a=int(input("enter 1:"))
+#b=int(input("enter2:"))
+#print("sum=",a+b)
+#division two real number
+a=int(input("enter 1:"))
+b=int(input("enter2:"))
+if ((type(a)==int or float)):
+ if ((type(b)==int or float)):
+  print("division",a/b)
+#dot product of two vectors a,b
+a=[1,2,3,4]
+b=[-1,7,2,1]
+c=[]
+print("c=")
+if (len(a)==len(b)):
+   for i in range (len(a)):
+           c[i]=a[i]*b[i]
+           print(c[i])
+           
+else :
+  printf("not a dot product")
+           
+           
+  
+
